081c9808e9edb7f71cf3bc3e2041c8a72bc780b5
류연우 <yyw980312@gmail.com>
2022-05-04 17:02:51 +0900

    Update tec-tac-toe.py
    변수명 변경
    구조 개선
    버그 수정
    버그 발견.
    
    OOO
      XX의 경우
    1열에 추가해야 끝남

--- games/2018180015/tec-tac-toe.py
+++ games/2018180015/tec-tac-toe.py
@@ -25,29 +25,29 @@ class tec_tac_toe:
         self.cells_frame = Frame(self.window)
         self.cells_frame.grid(row = 0, column = 0)
 
-        self.label_list = [Button(self.cells_frame, image = tec_tac_toe.empty, ) for i in range(9)]
-        self.label_list[0]["command"] = lambda: self.pressed(0)
-        self.label_list[1]["command"] = lambda: self.pressed(1)
-        self.label_list[2]["command"] = lambda: self.pressed(2)
-        self.label_list[3]["command"] = lambda: self.pressed(3)
-        self.label_list[4]["command"] = lambda: self.pressed(4)
-        self.label_list[5]["command"] = lambda: self.pressed(5)
-        self.label_list[6]["command"] = lambda: self.pressed(6)
-        self.label_list[7]["command"] = lambda: self.pressed(7)
-        self.label_list[8]["command"] = lambda: self.pressed(8)
+        self.button_list = [Button(self.cells_frame, image = tec_tac_toe.empty, ) for i in range(9)]
+        self.button_list[0]["command"] = lambda: self.pressed_callback(0)
+        self.button_list[1]["command"] = lambda: self.pressed_callback(1)
+        self.button_list[2]["command"] = lambda: self.pressed_callback(2)
+        self.button_list[3]["command"] = lambda: self.pressed_callback(3)
+        self.button_list[4]["command"] = lambda: self.pressed_callback(4)
+        self.button_list[5]["command"] = lambda: self.pressed_callback(5)
+        self.button_list[6]["command"] = lambda: self.pressed_callback(6)
+        self.button_list[7]["command"] = lambda: self.pressed_callback(7)
+        self.button_list[8]["command"] = lambda: self.pressed_callback(8)
 
-        for i, l in enumerate(self.label_list):
+        for i, l in enumerate(self.button_list):
             l.grid(row=i//3, column=i%3, sticky = 'n' )
 
-        self.label_1 = Label(self.window, text='O 차례')
-        self.label_1.grid(row = 1, column = 0)
+        self.text_label = Label(self.window, text='O 차례')
+        self.text_label.grid(row = 1, column = 0)
 
     def disable_all(self):
         for i in range(9):
-            self.label_list[i]["state"] = DISABLED
+            self.button_list[i]["state"] = DISABLED
 
     def finish(self):
-        self.label_1["text"] = self.end_text_front + self.end_text_behind
+        self.text_label["text"] = self.end_text_front + self.end_text_behind
         self.disable_all()
 
     def O_win(self):
@@ -61,43 +61,52 @@ class tec_tac_toe:
         self.finish()
 
     def judge(self):
-        if self.turn == 8:
+        if self.turn == 9:
             self.finish()
         for i in range(3):
             if self.cells[i*3] == self.cells[i*3+1] == self.cells[i*3+2]:
-                if self.cells[i*3] == 'o':
-                    self.O_win()
-                elif self.cells[i*3] == 'x':
-                    self.X_win()
+                self.who_is_winner(i*3)
                 break
             if self.cells[i] == self.cells[i+3] == self.cells[i+6]:
-                if self.cells[i] == 'o':
-                    self.O_win()
-                elif self.cells[i] == 'x':
-                    self.X_win()
+                self.who_is_winner(i)
                 break
         if self.cells[0] == self.cells[4] == self.cells[8] or self.cells[2] == self.cells[4] == self.cells[6]:
-            if self.cells[4] == 'o':
-                self.O_win()
-            elif self.cells[4] == 'x':
-                self.X_win()
+            self.who_is_winner( 4 )
 
-    def pressed(self, i):
-        if self.turn % 2 == 1:
-            self.label_list[i]["image"] = tec_tac_toe.i_X
-            self.label_list[i]["state"] = DISABLED
-            self.cells[i] = 'x'
-            self.label_1["text"] = "O 차례"
-        elif self.turn % 2 == 0:
-            self.label_list[i]["image"] = tec_tac_toe.i_O
-            self.label_list[i]["state"] = DISABLED
-            self.cells[i] = 'o'
-            self.label_1["text"] = "X 차례"
+    def who_is_winner( self, i ):
+        if self.cells[i] == 'o':
+            self.O_win()
+        elif self.cells[i] == 'x':
+            self.X_win()
 
+    def pressed_callback(self, i):
+        if self.cells[i] == ' ':
+            if self.turn % 2 == 1:
+                self.button_list[i]["image"] = tec_tac_toe.i_X
+                self.cells[i] = 'x'
+                self.text_label["text"] = "O 차례"
+            if self.turn % 2 == 0:
+                self.button_list[i]["image"] = tec_tac_toe.i_O
+                self.cells[i] = 'o'
+                self.text_label["text"] = "X 차례"
+            self.turn = self.turn + 1
+        else:
+            self.text_label["text"] = "다른 곳을 선택해 주세요"
         self.judge()
-        self.turn = self.turn + 1
+        self.for_debug()
+
+    def for_debug(self):
+        print( "trun : ", self.turn )
+        for i in range(3):
+            print(self.cells[i*3],self.cells[i*3+1],self.cells[i*3+2])
+
+    def start_game(self):
+        self.window.mainloop()
+
+    def end_game(self):
+        self.window.destroy()       # 사용할 곳이 있을까?
 
 
 game1 = tec_tac_toe()
 
-game1.window.mainloop()
+game1.start_game()
